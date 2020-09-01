f852fff4bd68bef23c186e945a781871976c0862
Evren Tukle <tukleevren@gmail.com>
2020-09-01 14:47:38 +0300

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Evren Tükle on 1.09.2020.
+//  Copyright © 2020 Evren Tükle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
